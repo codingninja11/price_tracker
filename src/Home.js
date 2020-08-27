876c7362810e858f086febb3041ed66a546e4ff9
codingninja11 <akritisheresth@gmail.com>
2020-08-27 15:48:50 +0530

    commit

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ function Home() {
      <div class="container">
        <div class="header"><h2 align="center">Price Tracker</h2></div>
        <div class="content">
-        <div> <h3 align="center">Free Price Tracker for Amazon</h3> </div>
+        <div> <h3 align="center">Price Tracker for Amazon</h3> </div>
         <div> <input type="text" placeholder="Enter URL" /> </div>
         <div> <input type="button" value="CHECK" class="btn" /> </div>
        </div>
